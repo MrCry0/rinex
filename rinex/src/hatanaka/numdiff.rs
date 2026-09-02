MrCry0/rinex

commit c9e57184d00a020194227cf3775e9adf8d99dae7

Upgrading hatanaka structures
Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

rinex/src/hatanaka/numdiff.rs
@@ -10,48 +10,60 @@ pub enum Error {
 }
 
 /// [NumDiff] is dedicated to numerical (de-)compression, following
-/// algorithm developped by Y. Hatanaka. This compression scheme
-/// is not lossless: the more efficient the data compression, the farther
-/// you will get from the oritinal data points.
-/// [NumDiff] is limited to M <= 6!! it will panic in higher orders.
-/// We recommend fixing M = 5 in the application:
-///    - because it was determined as optimal compromise
-///    - because you can only compress/decompress data that used the same M value,
-///    and M=5 is hardcoded in the historical RNX2CRX program. If you intend to
-///    use our library with general public files, they most likely went through
-///    that very program, and we need to remain compatible.
-/// Our API allows controlling the compression versus loss of accuracy compromise.
+/// the algorithm developped by Y. Hatanaka. This compression
+/// is not lossless: the more efficient the data compression, the bigger the error.
+/// M specifies the maximal compression to ever be supported by the object.
+/// The compression level may vary freely during the object's lifetime, but exceeding M
+/// will cause a panic. Note that m = 5 was determined as best compromise.
+/// [NumDiff] does not support M>6!! it will panic in higher orders.
+/// Set M = 6 in your application (when building the object) and you'll be fine.
+/// Note that m=3 seems to be hardcoded in the historical RNX2CRX program.
+/// If you want to produce compatible data, you should respect that.
+/// Note that we support m<=(M=6), therefore if you remain within our application,
+/// you can use higher compression order.
 #[derive(Debug, Clone)]
 pub struct NumDiff<const M: usize> {
     /// iteration counter
     m: usize,
+    /// compression level, within M maximal range
+    level: usize,
     /// internal data history
     buf: [i64; M],
 }
 
 impl<const M: usize> NumDiff<M> {
-    /// Builds a [NumDiff] structure dedicated to numerical (de-)compression
-    /// with `data` initial point.    
-    pub fn new(data: i64) -> Self {
+    /// Builds a [NumDiff] structure dedicated to numerical (de-)compression.
+    /// Level must not exceed 6 otherwise this will panic.
+    /// ## Inputs
+    ///  - data: initial point
+    ///  - level: compression level / range.
+    pub fn new(data: i64, level: usize) -> Self {
+        if level > 6 {
+            panic!("M=6 is the compression limit");
+        }
         let mut buf = [0; M]; // reset
         buf[0] = data;
-        Self { buf, m: 0 }
+        Self { buf, m: 0, level }
     }
     /// [NumDiff] needs to be reinit   when ???
-    pub fn force_init(&mut self, data: i64) {
+    pub fn force_init(&mut self, data: i64, level: usize) {
+        if level > 6 {
+            panic!("M=6 is the compression limit");
+        }
         self.m = 0;
+        self.level = level;
         self.rotate_history(data);
     }
 
     /// Rotate internal buffer, take new sample into account.
     fn rotate_history(&mut self, data: i64) {
-        self.buf.copy_within(1..M - 1, 0);
+        self.buf.copy_within(0..M - 2, 1);
         self.buf[0] = data;
     }
 
     /// Decompresses input data point, returns recovered data point.
     pub fn decompress(&mut self, data: i64) -> i64 {
-        if self.m < M - 1 {
+        if self.m < self.level {
             self.m += 1;
         }
 
@@ -69,7 +81,7 @@ impl<const M: usize> NumDiff<M> {
                     + 6 * self.buf[4]
                     - self.buf[5]
             },
-            _ => panic!("numdiff is limited to M<=6!"),
+            _ => panic!("numdiff is limited to M < 7"),
         };
 
         self.rotate_history(new);
@@ -78,7 +90,7 @@ impl<const M: usize> NumDiff<M> {
 
     /// Compresses input data point, returns "compressed" data point.
     pub fn compress(&mut self, data: i64) -> i64 {
-        if self.m < M - 1 {
+        if self.m < self.level {
             self.m += 1;
         }
         self.rotate_history(data);
@@ -99,7 +111,7 @@ impl<const M: usize> NumDiff<M> {
                     - 6 * self.buf[5]
                     + self.buf[6]
             },
-            _ => panic!("numdiff is limited to M<=6"),
+            _ => panic!("numdiff is limited to M < 7"),
         }
     }
 }
@@ -110,64 +122,44 @@ mod test {
 
     #[test]
     fn test_decompression() {
-        let mut diff = NumDiff::<6>::new(25065408994);
-        assert_eq!(diff.decompress(5918760), 25071327754);
-        assert_eq!(diff.decompress(92440), 25077338954);
-        assert_eq!(diff.decompress(-240), 25083442354);
-        assert_eq!(diff.decompress(-320), 25089637634);
-        assert_eq!(diff.decompress(-160), 25095924634);
-        assert_eq!(diff.decompress(-580), 25102302774);
-        assert_eq!(diff.decompress(360), 25108772414);
-        assert_eq!(diff.decompress(-1380), 25115332174);
-        assert_eq!(diff.decompress(220), 25121982274);
-        assert_eq!(diff.decompress(-140), 25128722574);
+        let mut diff = NumDiff::<6>::new(126298057858, 3);
+        assert_eq!(diff.decompress(-15603288), 126282454570);
+        assert_eq!(diff.decompress(521089), 126267372371);
+        assert_eq!(diff.decompress(-752), 126252810509);
+        assert_eq!(diff.decompress(1575419284), 127814188268);
+        assert_eq!(diff.decompress(-3150848707), 127800656941);
+        assert_eq!(diff.decompress(1575424909), 127787641437);
+        assert_eq!(diff.decompress(-135), 127775141621);
 
         // test re-init
-        diff.force_init(24701300559);
-        assert_eq!(diff.decompress(-19542118), 24681758441);
-        assert_eq!(diff.decompress(29235), 24662245558);
-        assert_eq!(diff.decompress(-38), 24642761872);
-        assert_eq!(diff.decompress(1592), 24623308975);
-        assert_eq!(diff.decompress(-931), 24603885936);
-        assert_eq!(diff.decompress(645), 24584493400);
-        assert_eq!(diff.decompress(1001), 24565132368);
-        assert_eq!(diff.decompress(-1038), 24545801802);
-        assert_eq!(diff.decompress(2198), 24526503900,);
-        assert_eq!(diff.decompress(-2679), 24507235983);
-        assert_eq!(diff.decompress(2804), 24488000855);
-        assert_eq!(diff.decompress(-892), 24468797624);
+        diff.force_init(111982965979, 3);
+        assert_eq!(diff.decompress(-16266911), 111966699068);
+        assert_eq!(diff.decompress(609858), 111951042015);
+        assert_eq!(diff.decompress(-213), 111935994607);
+        assert_eq!(diff.decompress(1575419307), 113496976151);
+        assert_eq!(diff.decompress(-3150848442), 113483138205);
+        assert_eq!(diff.decompress(1575425367), 113469906136);
+        assert_eq!(diff.decompress(146), 113457280090);
     }
 
     #[test]
     fn test_compression() {
-        let mut diff = NumDiff::<3>::new(25065408994);
-        assert_eq!(diff.compress(25071327754), 5918760);
-        assert_eq!(diff.compress(25077338954), 92440);
-        assert_eq!(diff.compress(25083442354), -240);
-        assert_eq!(diff.compress(25089637634), -320);
-        assert_eq!(diff.compress(25095924634), -160);
-        assert_eq!(diff.compress(25102302774), -580);
-        assert_eq!(diff.compress(25108772414), 360);
-        assert_eq!(diff.compress(25115332174), -1380);
-        assert_eq!(diff.compress(25121982274), 220);
-        assert_eq!(diff.compress(25128722574), -140);
+        let mut diff = NumDiff::<6>::new(126298057858, 3);
+        assert_eq!(diff.compress(126282454570), -15603288);
+        assert_eq!(diff.compress(126267372371), 521089);
+        assert_eq!(diff.compress(126252810509), -752);
+        assert_eq!(diff.compress(127814188268), 1575419284);
+        assert_eq!(diff.compress(127800656941), -3150848707);
+        assert_eq!(diff.compress(127787641437), 1575424909);
+        assert_eq!(diff.compress(127775141621), -135);
 
-        //TODO: test reinit
-
-        /*
-        let init : i64 = 126298057858;
-        diff.init(3, init)
-            .unwrap();
-        assert_eq!(diff.compress(25071327754), 5918760);
-        assert_eq!(diff.compress(25077338954), 92440);
-        assert_eq!(diff.compress(25083442354),-240);
-        assert_eq!(diff.compress(25089637634),-320);
-        assert_eq!(diff.compress(25095924634),-160);
-        assert_eq!(diff.compress(25102302774), -580);
-        assert_eq!(diff.compress(25108772414), 360);
-        assert_eq!(diff.compress(25115332174),-1380);
-        assert_eq!(diff.compress(25121982274), 220);
-        assert_eq!(diff.compress(25128722574),-140);
-        */
+        diff.force_init(111982965979, 3);
+        assert_eq!(diff.compress(111966699068), -16266911);
+        assert_eq!(diff.compress(111951042015), 609858);
+        assert_eq!(diff.compress(111935994607), -213);
+        assert_eq!(diff.compress(113496976151), 1575419307);
+        assert_eq!(diff.compress(113483138205), -3150848442);
+        assert_eq!(diff.compress(113469906136), 1575425367);
+        assert_eq!(diff.compress(113457280090), 146);
     }
 }

rinex/src/hatanaka/compressor.rs
@@ -20,7 +20,7 @@ struct ObsKey {
 }
 
 /// Structure to compress RINEX data
-pub struct Compressor {
+pub struct Compressor<const M: usize> {
     /// finite state machine
     state: State,
     /// True until END OF HEADER has not been reached
@@ -42,9 +42,9 @@ pub struct Compressor {
     /// Epoch [TextDiff]
     epoch_diff: TextDiff,
     /// Clock [NumDiff]
-    clock_diff: NumDiff<3>,
+    clock_diff: NumDiff<M>,
     /// SV Diff
-    sv_diff: HashMap<ObsKey, ObsDiff<3>>,
+    sv_diff: HashMap<ObsKey, ObsDiff<M>>,
     /// Pending kernel re-initialization
     forced_init: HashMap<SV, Vec<usize>>,
 }
@@ -59,7 +59,7 @@ fn format_epoch_descriptor(content: &str) -> String {
     result
 }
 
-impl Default for Compressor {
+impl<const M: usize> Default for Compressor<M> {
     fn default() -> Self {
         Self {
             first_epoch: true,
@@ -74,12 +74,12 @@ impl Default for Compressor {
             epoch_diff: TextDiff::new(""),
             sv_diff: HashMap::new(),
             forced_init: HashMap::new(),
-            clock_diff: NumDiff::<3>::new(0),
+            clock_diff: NumDiff::<M>::new(0, 3),
         }
     }
 }
 
-impl Compressor {
+impl<const M: usize> Compressor<M> {
     /// Identifies amount of vehicles to be provided in next iterations
     /// by analyzing epoch descriptor
     fn determine_nb_vehicles(&self, content: &str) -> Result<usize, Error> {
@@ -159,7 +159,7 @@ impl Compressor {
             .filter_map(|(key, value)| if key == obskey { Some(value) } else { None })
             .reduce(|k, _k| k)
         {
-            kernel.force_init(obsdata, snr, lli);
+            kernel.force_init(obsdata, 3, snr, lli);
         }
     }
 

rinex/src/hatanaka/decompressor.rs
@@ -354,7 +354,7 @@ impl<const M: usize, R: Read> Decompressor<M, R> {
             crinex_found: false,
             crinex: CRINEX::default(),
             epoch_diff: TextDiff::new(""),
-            clock_diff: NumDiff::<M>::new(0),
+            clock_diff: NumDiff::<M>::new(0, 3),
             obs_diff: HashMap::new(), // init. late
             buf_ascii: String::with_capacity(128),
             epoch_ascii: String::with_capacity(256),

rinex/src/hatanaka/obs.rs
@@ -9,16 +9,16 @@ pub struct ObsDiff<const M: usize> {
 }
 
 impl<const M: usize> ObsDiff<M> {
-    pub fn new(obsdata: i64, snr: &str, lli: &str) -> Self {
+    pub fn new(obsdata: i64, obsdata_level: usize, snr: &str, lli: &str) -> Self {
         Self {
             snr_diff: TextDiff::new(snr),
             lli_diff: TextDiff::new(lli),
-            data_diff: NumDiff::<M>::new(obsdata),
+            data_diff: NumDiff::<M>::new(obsdata, obsdata_level),
         }
     }
-    pub fn force_init(&mut self, obsdata: i64, snr: &str, lli: &str) {
+    pub fn force_init(&mut self, obsdata: i64, obsdata_level: usize, snr: &str, lli: &str) {
         self.snr_diff.force_init(snr);
         self.lli_diff.force_init(lli);
-        self.data_diff.force_init(obsdata);
+        self.data_diff.force_init(obsdata, obsdata_level);
     }
 }

rinex/src/hatanaka/textdiff.rs
@@ -1,15 +1,14 @@
 //! Y. Hatanaka lossless TextDiff algorithm
 
-// TODO
-// [TextDiff] does not support Compress/Decompress back and forth,
-// it currently requires to be built for one way or the other,
-// and requires new Build to change direction.
-// We should avoid that and permit back & forth operations.
-
+/// [TextDiff] is a structure that implements the Text diff. algorithm
+/// designed by Y. Hatanaka, which is a lossless text compression algorithm.
+/// [TextDiff] in its current form does not allow compressing & decompressing (back & forth)
+/// at the same time: you need two dedicated objects.
+/// This does not bother our application because it only operates in one way,
+/// but it is one aspect to keep in mind.
 #[derive(Debug)]
 pub struct TextDiff {
     buffer: String,
-    // avoids one alloc per compression call
     compressed: String,
 }
 
@@ -86,14 +85,20 @@ impl TextDiff {
         unsafe {
             let mut compressed = self.compressed.as_bytes_mut().iter_mut();
 
-            // any new byte gets compressed or kept
+            // run through bytes for which we have history
+            // and either keep or compress
             while let Some(new) = new.next() {
                 let compressed = compressed.next().unwrap();
+
                 if let Some(history) = history.next() {
                     if history == new {
                         *compressed = b' ';
                     } else {
-                        *compressed = new;
+                        if new == b' ' {
+                            *compressed = b'&';
+                        } else {
+                            *compressed = new;
+                        }
                     }
                 }
             }
@@ -189,6 +194,8 @@ mod test {
         assert_eq!(diff.compress("4 "), " &");
         assert_eq!(diff.compress("4  "), "  &");
         assert_eq!(diff.compress("0"), "0  ");
+        assert_eq!(diff.compress("0"), "   ");
+        assert_eq!(diff.compress("   "), "&  ");
 
         diff.force_init("Default 1234");
         assert_eq!(diff.compress("DEfault 1234"), " E          ");

commit 9944b319e21e55a9de686b7fbbe0311a66a997f9

hatanaka: fix history buffer rotation in NumDiff
rotate_history() shifted only buf[0..M-2] into buf[1..M-1], leaving
the last slot buf[M-1] untouched after construction. Any decompress()
or compress() call that reaches the maximal supported order M (order
5 for the default NumDiff<5> used to decompress the CRINEX clock
offset field) then read that stale, never-updated slot instead of the
correct historical value.

Because Hatanaka differencing is stateful, the resulting error is not
self-correcting: it propagates into every following sample and grows
unbounded, eventually causing an arithmetic overflow panic deep into
otherwise valid CRINEX V3 files that legitimately reach that order.

Shift buf[0..M-1] instead, so every slot of the history buffer stays
current.

Fixes: c9e57184d00a ("Upgrading hatanaka structures")
Closes: https://github.com/nav-solutions/rinex/issues/426
Signed-off-by: Oleksandr Suvorov <cryosay@gmail.com>

rinex/src/hatanaka/numdiff.rs
@@ -48,7 +48,7 @@ impl<const M: usize> NumDiff<M> {
 
     /// Rotate internal buffer, take new sample into account.
     fn rotate_history(&mut self, data: i64) {
-        self.buf.copy_within(0..M - 2, 1);
+        self.buf.copy_within(0..M - 1, 1);
         self.buf[0] = data;
     }
 
@@ -152,5 +152,58 @@ mod test {
         assert_eq!(diff.compress(113483138205), -3150848442);
         assert_eq!(diff.compress(113469906136), 1575425367);
         assert_eq!(diff.compress(113457280090), 146);
+    }
+
+    #[test]
+    fn test_history_rotation_full_order() {
+        // Regression test for https://github.com/nav-solutions/rinex/issues/426
+        //
+        // `rotate_history` used to only shift `buf[0..M-2]`, so `buf[M-1]`
+        // (the oldest history slot) was frozen at its initial value forever
+        // and never took part in the running history. Decompression that
+        // reaches the maximal order `M` (as happens for the clock-offset
+        // field, which is decompressed with `NumDiff<5>` at order 5) then
+        // silently used a stale value instead of the correct one, causing
+        // decompression to diverge and eventually overflow further down
+        // the file.
+        //
+        // `compressed` below is the correct order-5 CRINEX encoding of
+        // `original`, computed independently of this crate. Decompressing
+        // it must reproduce `original` exactly, which requires every slot
+        // of the history buffer (including buf[M-1]) to stay up to date.
+        let original = [
+            126298057858_i64,
+            126282454570,
+            126267372371,
+            126252810509,
+            127814188268,
+            127800656941,
+            127787641437,
+            127775141621,
+            127762669477,
+        ];
+        let compressed = [
+            -15603288_i64,
+            521089,
+            -752,
+            1575420036,
+            -6301688027,
+            9452541607,
+            -6301698660,
+            1574937163,
+        ];
+
+        let mut diff = NumDiff::<5>::new(original[0], 5);
+
+        let mut recovered = vec![original[0]];
+        for value in compressed {
+            recovered.push(diff.decompress(value));
+        }
+
+        assert_eq!(
+            recovered, original,
+            "decompressed sequence diverged from the original: \
+             history rotation must keep every buffer slot (including buf[M-1]) up to date"
+        );
     }
 }